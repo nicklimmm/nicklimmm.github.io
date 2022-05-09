0eabded5b41d1a7c53d7eb046349ea635251ff70
nicklimmm <nickylim.p@gmail.com>
2022-05-09 18:08:22 +0700

    added work experience

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import { Layout } from "../components/Layout";
 import { Hero } from "../components/Hero";
 import "animate.css";
 import { Education } from "../components/Education";
+import { WorkExperience } from "../components/WorkExperience";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
       <Education />
+      <WorkExperience />
     </Layout>
   );
 }
